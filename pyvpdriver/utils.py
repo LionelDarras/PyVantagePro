22a11fe0010af11edac4d4440952bde584067efc
residante <mr.residante@gmail.com>
2012-05-22 15:13:29 +0200

    add cached_properties decorator.

--- pyvpdriver/utils.py
+++ pyvpdriver/utils.py
@@ -7,3 +7,32 @@
     :license: GNU GPL v3.
 
 """
+
+import time
+
+class cached_property(object):
+    """A decorator that converts a function into a lazy property. The
+    function wrapped is called the first time to retrieve the result
+    and then that calculated result is used the next time you access
+    the value."""
+    def __init__(self, func, doc=None, ttl=300):
+        self.func = func
+        self.__doc__ = doc or func.__doc__
+        self.__name__ = func.__name__
+        self.__module__ = func.__module__
+        self.ttl = ttl
+
+    def __get__(self, inst, owner):
+        now = time.time()
+        try:
+            value, last_update = inst._cache[self.__name__]
+            if self.ttl > 0 and now - last_update > self.ttl:
+                raise AttributeError
+        except (KeyError, AttributeError):
+            value = self.func(inst)
+            try:
+                cache = inst._cache
+            except AttributeError:
+                cache = inst._cache = {}
+            cache[self.__name__] = (value, now)
+        return value
